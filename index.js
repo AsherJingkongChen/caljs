f468fdc9e6a1e9436d5d479dd1eac995e2b51dc2
ChenYiran <37398747+AsherJingkongChen@users.noreply.github.com>
2021-11-09 18:32:39 +0800

    Create index.js

---
+++ index.js
@@ -0,0 +1,22 @@
+function cal(quote){
+  strToArray=[]
+  scan=""
+  for(singleWord of quote){
+    sw=singleWord
+    if(sw=="+"||sw=="-"||sw=="x"||sw=="/"||sw=="("||sw==")"){
+      if(scan){
+        strToArray.push(scan)
+      }
+      if(sw=="x"){sw="*"}
+      strToArray.push(sw)
+      scan=""
+    }else{
+      scan+=sw
+    }
+  }
+  Str=strToArray.join(" ")
+  return eval(Str)
+}
+console.log(cal("2x(3+10)"))
+console.log(eval("2 * ( 3 + 10 )"))
+module.exports = cal //模組化 可忽略
